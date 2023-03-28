c88330e58307b8b4aab8f99d050570917845383f
le duong hien thong <michaeldiavolos@gmail.com>
2023-03-27 21:42:48 -0400

    test commit

--- homeaway/src/App.js
+++ homeaway/src/App.js
@@ -1,47 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-
-import { useEffect, useRef, useState } from 'react';
-
-gsap.registerPlugin(ScrollTrigger)
 
 function App() {
-
-
-  const logoRef = useRef(null)
-
-  useEffect(() => {
-
-    const el = logoRef.current
-
-    gsap.to(el, {
-      rotate: 360,
-      duration: 2,
-      scrollTrigger: {
-        trigger: el,
-        start: 'top 50%',
-        markers: true
-      }
-    })
-
-  }, [])
-
-
-
-
   return (
     <div className="App">
-     
-    <header>Top of page</header>
-
-    <section>
-      <img src='logo192.png' alt='img' ref={logoRef}></img>
-    </section>
-    
-    <footer>Bot of page</footer>
-
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
